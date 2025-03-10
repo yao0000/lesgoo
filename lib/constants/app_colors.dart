a10bcbad5f45a87a1df899436b65622c1df1f29d
Yao <cyaoen@gmail.com>
2025-03-10 22:36:30 +0800

    assets

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bgColor = Color(0xFFD6E4F0);
+  static const Color navyBlue = Color(0xFF0A1F44);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color orange = Colors.orange;
+}
