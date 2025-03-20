8834c346d004a24726b8e437176e7d22b962a99b
Yao <cyaoen@gmail.com>
2025-03-20 20:40:33 +0800

    global variable

---
+++ lib/data/global.dart
@@ -0,0 +1,24 @@
+import 'package:travel/data/models/user_model.dart';
+import 'package:travel/data/repositories/user_repository.dart';
+import 'package:travel/services/firebase_auth_service.dart';
+
+class Global {
+  static final Global _instance = Global._internal();
+  factory Global() => _instance;
+  Global._internal();
+
+  static final UserModel user = UserModel(uid: '', username: '', name: '', email: '', country: '', gender: '', phone: '', role: 'user');
+
+  static Future<void> loadUserInfo() async {
+    UserModel? currentUser = await UserRepository.getUser(AuthService.getCurrentUser()!.uid);
+    user.uid = currentUser!.uid;
+    user.username = currentUser.username;
+    user.name = currentUser.name;
+    user.email = currentUser.email;
+    user.country = currentUser.country;
+    user.gender = currentUser.gender;
+    user.phone = currentUser.phone;
+    user.role = currentUser.role;
+    user.imageUrl = currentUser.imageUrl;
+  }
+}
